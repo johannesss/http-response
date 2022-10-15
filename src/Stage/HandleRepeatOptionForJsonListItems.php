dc2f087d071a6b7c7ccd77de9227d9dacdacba6b
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2022-10-15 11:08:30 +0200

    refactor: small refactor

--- src/Stage/HandleRepeatOptionForJsonListItems.php
+++ src/Stage/HandleRepeatOptionForJsonListItems.php
@@ -10,6 +10,8 @@ class HandleRepeatOptionForJsonListItems implements StageInterface
 {
     public const KEY_REPEAT = '__repeat';
 
+    protected const MAX_REPEAT = 50;
+
     protected $body;
 
     protected $structures = [];
@@ -45,36 +47,37 @@ class HandleRepeatOptionForJsonListItems implements StageInterface
         );
 
         foreach ($iterator as $key => $value) {
-            if (is_array($value) && array_key_exists(self::KEY_REPEAT, $value)) {
+            if (!is_array($value) || is_array($value) && !array_key_exists(self::KEY_REPEAT, $value)) {
+                continue;
+            }
 
-                $item         = $value;
-                $currentDepth = $iterator->getDepth() - 1;
+            $item         = $value;
+            $currentDepth = $iterator->getDepth() - 1;
 
-                for ($subDepth = $currentDepth; $subDepth >= 0; $subDepth--) {
+            for ($subDepth = $currentDepth; $subDepth >= 0; $subDepth--) {
 
-                    $subIterator = $iterator->getSubIterator($subDepth);
+                $subIterator = $iterator->getSubIterator($subDepth);
 
-                    $subKey = $subIterator->key();
+                $subKey = $subIterator->key();
 
-                    $repeat = $this->limit(50, $item[self::KEY_REPEAT]);
-                    $repeat = $repeat < 0 ? 1 : $repeat;
+                $repeat = $this->limit(self::MAX_REPEAT, $item[self::KEY_REPEAT]);
+                $repeat = $repeat < 0 ? 1 : $repeat;
 
-                    $value = $subIterator->offsetGet($subKey);
-                    // delete the item we're repeating to make it easier
-                    // when adding repeat items
-                    unset($value[$key]);
-                    $value = array_merge(
-                        $value, array_fill(0, $repeat, $item)
-                    );
+                $value = $subIterator->offsetGet($subKey);
+                // delete the item we're repeating to make it easier
+                // when adding repeat items
+                unset($value[$key]);
+                $value = array_merge(
+                    $value, array_fill(0, $repeat, $item)
+                );
 
-                    $subIterator->offsetSet(
-                        $subKey,
-                        ($subDepth === $currentDepth ?
-                            $value :
-                            $iterator->getSubIterator($subDepth + 1)->getArrayCopy()
-                        )
-                    );
-                }
+                $subIterator->offsetSet(
+                    $subKey,
+                    ($subDepth === $currentDepth ?
+                        $value :
+                        $iterator->getSubIterator($subDepth + 1)->getArrayCopy()
+                    )
+                );
             }
         }
 
